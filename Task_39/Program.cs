47ab734554af3b7104a624a2ab3d8e7c8e1dc464
Margarita <margarita_anis@bk.ru>
2022-06-04 15:36:41 +0300

    Add project Task_39

--- Task_39/Program.cs
+++ Task_39/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая перевернёт одномерный массив 
+// (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+
+Console.Write("Введите целые числа через пробел: ");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+int index1 = 0;
+int index2 = array.Length - 1;
+
+while (index1 < index2)
+{
+    int temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp; 
+
+    index1++;
+    index2--;
+}
+
+Console.Write($"{String.Join(" ", array)}");
+
+
