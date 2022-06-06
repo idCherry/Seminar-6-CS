d74ddcc053519b1916a6a33ee61aebd430d54db8
Margarita <margarita_anis@bk.ru>
2022-06-06 21:03:19 +0300

    Add project Task_42

---
+++ Task_42/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+
+Console.Write("Ведите целое число: ");
+int num = int.Parse(Console.ReadLine());
+Console.Write("Ведите систему счисления: ");
+int baseNum = int.Parse(Console.ReadLine());
+
+// int result = 0;
+// int d10 = 1;
+
+// while (num != 0)
+// {
+//     result = result + num % baseNum * d10;
+//     num /= baseNum;
+//     d10 *= 10;
+// }
+// Console.WriteLine(result);
+
+void DecToOther(int n, int baseN)
+{
+    if (n == 0) return;
+    DecToOther (n / baseN, baseN);
+    Console.Write(n % baseN);
+}
+DecToOther(num, baseNum);
