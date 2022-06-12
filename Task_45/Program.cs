750a4b3da03e73a19b0af5a18e7cd2c2a1a719b9
Margarita <margarita_anis@bk.ru>
2022-06-12 12:59:00 +0300

    Add project Task_45

---
+++ Task_45/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая будет создавать копию массива с помощью поэлементного копирования.
+
+Console.Write("Ваедите размер массива: ");
+int size = int.Parse(Console.ReadLine());
+
+int[] array = new int[size];
+int[] newArray = new int[size];
+
+for (int i = 0; i < size; i++)
+{
+    array[i] = new Random().Next(-50, 50);
+}
+
+for (int i = 0; i < size; i++)
+{
+    newArray[i] = array[i];
+}
+
+Console.WriteLine($"{string.Join(' ', array)}");
+Console.WriteLine($"{string.Join(' ', newArray)}");
