71e20e357d3a9a400d2b60bb1ea3f82b15e7ab64
Margarita <margarita_anis@bk.ru>
2022-06-12 12:41:44 +0300

    Add project Task_44

---
+++ Task_44/Program.cs
@@ -0,0 +1,17 @@
+﻿// Не используя рекурсию, выведите N чисел Фибоначчи. Первые два числа Фибоначчи: 0 и 1.
+
+
+Console.WriteLine("Введите ограничение кол-ва цифр Фибоначчи: ");
+int number = int.Parse(Console.ReadLine());
+int a = 0;
+int b = 1;
+Console.Write(a + " " + b + " ");
+int sum = 0;
+
+for (int i = 0; i < number - 2; i++)
+{
+    sum = a + b;
+    Console.Write(sum + " ");
+    a = b;
+    b = sum;
+}
