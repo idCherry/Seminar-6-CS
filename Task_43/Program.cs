abdb1d86c5c748fa6e61c94e105e0e3533b5dce6
Margarita <margarita_anis@bk.ru>
2022-06-13 11:07:10 +0300

    Fix mistakes HOMEWORK Task_43

--- Task_43/Program.cs
+++ Task_43/Program.cs
@@ -2,19 +2,19 @@
 // y = k1 * x + b1, y = k2 * x + b2
 
 Console.Write("Задайте значение b1: ");
-int b1 = int.Parse(Console.ReadLine());
+double b1 = double.Parse(Console.ReadLine());
 
 Console.Write("Задайте значение k1: ");
-int k1 = int.Parse(Console.ReadLine());
+double k1 = double.Parse(Console.ReadLine());
 
 Console.Write("Задайте значение b2: ");
-int b2 = int.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
 
 Console.Write("Задайте значение k2: ");
-int k2 = int.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
 
-int x1 = (b2 - b1) / (k1 - k2);
-int result = k1 * x + b1;
+double x = (b2 - b1) / (k1 - k2);
+double result = k1 * x + b1;
 
 Console.WriteLine();
 Console.Write($"Точка пересечения: ( {x}; {result} )");
