1447a72448367b8461b5b34b3c3e99109840916d
Margarita <margarita_anis@bk.ru>
2022-06-13 11:12:46 +0300

    Fix mistakes HOMEWORK Task_41

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,25 +1,16 @@
 ﻿// Пользователь вводит с клавиатуры М чисел.
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
-Console.Write("Введите размер массива: ");
-int size = int.Parse(Console.ReadLine());
+
+Console.Write("Введите элементы массива через пробел: ");
+int [] numsArr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 int count = 0;
-
-int[] array = new int[size];
-
-Console.Write("Массив [ ");
-for (int i = 0; i < size; i++)
-{
-    array[i] = new Random().Next(-10, 10);
-    Console.Write(array[i] + " ");
-}
-Console.Write("]");
 
 Console.WriteLine();
 
-for (int i = 0; i < size; i++)
+for (int i = 0; i < numsArr.Length; i++)
 {
-    if (array[i] > 0) count += 1;
+    if (numsArr[i] > 0) count += 1;
 }
 
 Console.WriteLine($"Количество чисел больше нуля в массиве = {count}");
